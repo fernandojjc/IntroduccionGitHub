72db06ce598c35ff845616a1b01ae7dcf9224ad9
Juárez Chávez José Fernando <fernandojuarezchavez123@gmail.com>
2021-02-19 01:11:43 -0600

    Esta es la primera versión2

---
+++ IntroduccionGitHub.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package introducciongithub;
+
+/**
+ *
+ * @author Jose Fernando Juarez Chavez
+ */
+public class IntroduccionGitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Primer proyecto en GitHub");
+    }
+    
+}
